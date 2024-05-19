5af917e29ab368a1e2055948c93f868406d302de
Jay Tiwari <tiwarijai999@gmail.com>
2024-05-19 16:10:18 +0530

    finished

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const express =require('express');
 const cors=require('cors');
-const { default: mongoose } = require('mongoose');
+const { default: mongoose, Schema } = require('mongoose');
 const User=require('./models/User');
+const Post=require('./models/Post');
 const bcrypt= require('bcryptjs'); // bcrypt is used to encoide the password which is been saved in the data base
 const app=express();
  const jwt=require('jsonwebtoken');
 const cookieParser= require('cookie-parser');
 const multer =require('multer');// Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files. It is written on top of busboy for maximum efficiency.
-const uploadMiddleware=multer({dest:'uploads/'});
+const uploadMiddleware=multer({dest: 'uploads/' });
+const fs =require('fs');
 const salt = bcrypt.genSaltSync(10);
 
 
@@ -17,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 // for parsinng the request from the json we will use another middle ware that uis been called as the express.json
 
-
+app.use('/uploads',express.static(__dirname+ '/uploads'));
 
  mongoose.connect('mongodb://localhost:27017');
 app.post('/register',async (req,res)=>{
@@ -68,10 +70,93 @@ app.post('/logout',(req,res)=>{
 });
 
 
-app.post('/post',uploadMiddleware.single('file'),async (req,res)=>{
+app.post('/post',uploadMiddleware.single('file'),async  (req,res)=>{
    //saved file type is binary
-    await res.json({files:req.file}); // here there was a mistake we will write file not files
+    const {originalname,path}=req.file;
+    const parts= originalname.split('.');
+   
+    const extension= parts[parts.length-1]; 
+    const newPath=path+'.'+extension;
+    fs.renameSync(path,newPath);
 
+
+    const {token}=req.cookies;
+    jwt.verify(token,secret,{},async(err,info)=>{
+      if(err) throw err;
+ const{title,summary,content}=req.body;
+ const postDoc= await Post.create({
+      title,
+      summary,
+      content,
+      cover:newPath,
+      author: info.id,
+  });
+   res.json(postDoc);
+
+   });
+});
+
+
+
+
+   
+     
+   // here there was a mistake we will write file not files
+  
+   
+
+app.get('/post',async(req,res)=>{
+ res.json(await Post.find()
+ .populate('author',['username'])
+ .sort({createdAt:-1})
+ .limit(20)
+
+);
+});
+
+
+
+app.get('/post/:id',async(req,res)=>{
+   const {id}=req.params;
+
+   const postDoc= await Post.findById(id).populate('author',['username']);
+   res.json(postDoc);
+})
+
+
+app.put('/post',uploadMiddleware.single('file'),async(req,res)=>{
+   let newPath=null;
+   if(req.file){
+      const {originalname,path}=req.file;
+      const parts= originalname.split('.');
+     
+      const extension= parts[parts.length-1]; 
+       newPath=path+'.'+extension;
+      fs.renameSync(path,newPath);
+  
+   }
+   const {token}=req.cookies;
+   jwt.verify(token,secret,{},async(err,info)=>{
+      if(err) throw err;
+ const{id,title,summary,content}=req.body;
+      const postDoc=await Post.findById(id);
+      const isAuthor= JSON.stringify(postDoc.author)=== JSON.stringify(info.id);
+
+      if(!isAuthor){
+         return res.json(400).json('YOU ARE NOT THE AUTHOR');
+         
+      }
+      await postDoc.updateOne({
+         title,
+         summary,
+         content,
+         cover: newPath? newPath: postDoc.cover,
+      });
+
+   res.json(postDoc);
+
+
+}) ;
 });
 
 app.listen(4000);  
